7ba5748548e4be5e1dae8cdb1109140341aa6cbc
Ricky Aditya <rickyda79@gmail.com>
2020-06-17 16:32:04 +0700

    Re-adding files

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { urlencoded, json } from 'body-parser';
+import cors from 'cors';
+import routes from './routes';
+import "@babel/polyfill"
+const app = express();
+
+app.use(urlencoded({ extended: true }));
+app.use(json());
+app.use(cors());
+app.options('*', cors());
+routes(app);
+
+// start server and listen on port 5500
+const server = app.listen(5500, () => {
+  const port = server.address().port
+  console.log("app listening at ", port);
+});
+
