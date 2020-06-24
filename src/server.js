3f25329ef920d3af1b3e0a21be866f30d4ef0095
Ricky Aditya <rickyda79@gmail.com>
2020-06-24 14:29:22 +0700

    Add static dir module

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import { urlencoded, json } from 'body-parser';
 import cors from 'cors';
 import routes from './routes';
-import "@babel/polyfill"
+import serveIndex from 'serve-index';
+import "@babel/polyfill";
 const app = express();
 
 app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use(cors());
+app.use('/.well-known', express.static('.well-known'), serveIndex('.well-known'));
 app.options('*', cors());
 routes(app);
 
